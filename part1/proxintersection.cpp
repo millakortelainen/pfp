46543f42e1c672e01fef9c96c445ff6d1dc5d0e4
millakortelainen <milla.kortelainen@helsinki.fi>
2021-04-05 19:03:07 +0300

    update week1

--- part1/proxintersection.cpp
+++ part1/proxintersection.cpp
@@ -1,5 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include "Utils.hpp"
+#include <tuple>
+
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    int radius = stoi(argv[1]);
+
+    string fileName = argv[2];
+
+    ifstream f(fileName);
+    char a;
+    f >> a;
+    cout << a;
+    int b;
+    f >> b;
+    cout << b;
+    char c;
+    f >> c;
+    cout << c;
+    int d;
+    f >> d;
+    cout << d;
+    f.close();
     return 0;
 }
